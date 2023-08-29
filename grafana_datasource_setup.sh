d76711a2b8720eefd0fe7f2df7e7bc3281f262d8
TeknoPathshala <95009959+TeknoPathshala@users.noreply.github.com>
2023-08-29 16:11:50 +0530

    Update grafana_datasource_setup.sh

--- grafana_datasource_setup.sh
+++ grafana_datasource_setup.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-GRAFANA_URL="http://192.168.1.253:3000"
-GRAFANA_API_KEY="your_grafana_api_key"
-
 # Create InfluxDB data source
 curl -X POST "${GRAFANA_URL}/api/datasources" \
-     -H "Authorization: Bearer ${GRAFANA_API_KEY}" \
-     -H "Content-Type: application/json" \
-     --data '{
-         "name": "InfluxDB",
-         "type": "influxdb",
-         "access": "proxy",
-         "url": "http://192.168.1.253:8086",
-         "database": "teknopathshala",
-         "user": "tekno",
-         "password": "password@12"
-     }'
+    -H "Authorization: Bearer ${GRAFANA_API_KEY}" \
+    -H "Content-Type: application/json" \
+    --data '{
+        "name": "InfluxDB",
+        "type": "influxdb",
+        "access": "proxy",
+        "url": "http://'${INFLUXDB_SERVER_IP}':8086",
+        "database": "'${INFLUXDB_DATABASE}'",
+        "user": "'${INFLUXDB_USER}'",
+        "password": "'${INFLUXDB_PASSWORD}'"
+    }'
